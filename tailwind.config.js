9bb6edc6df2b9e3a6a26b547d0f171872e84722a
haoken35 <22520406@gm.uit.edu.vn>
2026-03-29 14:55:23 +0700

    adding framer-motion

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      xl: { max: "1400px" },
+      lg: { max: "1150px" },
+      md: { max: "767px" },
+      sm: { max: "639px" },
+    },
+  },
+  plugins: [],
+};

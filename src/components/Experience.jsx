3d396b2ec7baa9078c68d435beef859dd9906d16
haoken35 <22520406@gm.uit.edu.vn>
2026-03-31 10:03:18 +0700

    add About section

---
+++ src/components/Experience.jsx
@@ -0,0 +1,12 @@
+"use client";
+import Heading from "./sub/Heading";
+
+const Experience = () => {
+  return (
+    <div>
+      <Heading text={"Experience & Education"} />
+    </div>
+  );
+};
+
+export default Experience;
